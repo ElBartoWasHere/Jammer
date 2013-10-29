0c2c4444876ba3f992400b4024afba338d01f069
SignalShield <arturo.torres@upr.edu>
2013-10-28 23:02:11 -0700

    Before system integration LCD.c
    Running with Vcc connected to MSP430 GPIO. Turn off/on function controlled by MCU, program runs, LCD turns on for approximately 3 seconds and turns off. MCU goes to low-power mode. Next step is to implement interrupt with button, so when the button is pressed, LCD will turn on for 3 seconds displaying data, then turn off, and MCU goes into low-power mode with interrupt enabled for next time button is pressed, repeating same action.

--- LCD.c
+++ LCD.c
@@ -8,11 +8,11 @@
 //             |     LCD    |  
 //  LCD Pin #  |  Function  |   MSP Pin #
 //  ----------------------------
-//  PIN 1      |     GND    |     ---
-//  PIN 2      |     VCC    |     ---
-//  PIN 3      |     V0     |     ---
+//  PIN 1      |     GND    |     DVSS
+//  PIN 2      |     VCC    |     P4.5
+//  PIN 3      |     V0     |     Outer potentiometer
 //  PIN 4      |     RS     |     P4.6
-//  PIN 5      |     RW     |     ---
+//  PIN 5      |     RW     |     DVSS
 //  PIN 6      |     EN     |     P4.7
 //  PIN 7      |     DB0    |     P5.0
 //  PIN 8      |     DB1    |     P5.1
@@ -22,8 +22,8 @@
 //  PIN 12     |     DB5    |     P5.5     
 //  PIN 13     |     DB6    |     P5.6     
 //  PIN 14     |     DB7    |     P5.7     
-//  PIN 15     |     LED+   |     ---
-//  PIN 16     |     LED-   |     ---
+//  PIN 15     |     LED+   |     P4.5
+//  PIN 16     |     LED-   |     DVSS
 //
 //
 //
@@ -42,6 +42,7 @@
 #define         LCD_OUT                 P5OUT
 
 // Define LCD - MSP pin mapping 
+#define         LCD_VCC                 BIT5          // P4.5
 #define         LCD_PIN_RS              BIT6          // P4.6
 #define         LCD_PIN_EN              BIT7          // P4.7
 #define         LCD_PIN_D0              BIT0          // P5.0
@@ -64,24 +65,17 @@
 */
 void enable()
 {
-    
-    // pull Enable bit low
-    LCD_COM_OUT &= ~LCD_PIN_EN;
-    __delay_cycles(200);
-
     // pull Enable bit high
     LCD_COM_OUT |= LCD_PIN_EN;
-    __delay_cycles(200);
-
+    __delay_cycles(2000);
+    
     // pull Enable bit low again
     LCD_COM_OUT &= ~LCD_PIN_EN;
-    __delay_cycles(200);
+
 }
 
 /*
-* Description: Send a byte on the data bus in the 4 bit mode. This requires 
-*              sending data in two chunks, the high chunk first and then the low
-*              chunk.
+* Description: Send a byte on the data bus.
 * Parameters
 *    - byteToSend: the entire byte to send
 *    - isData: set TRUE if byte is character, FALSE if command
@@ -92,50 +86,36 @@ void sendByte(char byteToSend, int isData)
     LCD_COM_OUT &= ~LCD_COM_MASK;
     LCD_OUT &= ~LCD_PIN_MASK;
 
-    // set High Nibble (HN) - 
-    // usefulness of the identity mapping
-    // apparent here. We can set the 
-    // DB7 - DB4 just by setting P1.7 - P1.4 
-    // using a simple assignment
-    //LCD_OUT |= (byteToSend & 0xF0);
+    // set data for output
     LCD_OUT |= byteToSend;
- 
+    
+    // determine if instruction is command or character
     if (isData == TRUE)
     {
-        //LCD_OUT |= LCD_PIN_RS;
         LCD_COM_OUT |= LCD_PIN_RS;
     }
     else
     {
-        //LCD_OUT &= ~LCD_PIN_RS;
         LCD_COM_OUT &= ~LCD_PIN_RS;
     }
 
     // we've set up the input voltages to the LCD.
     // Now tell it to read them.
     enable();
+    
+}
+
 /*
-    // set Low Nibble (LN) -
-    // usefulness of the identity mapping
-    // apparent here. We can set the 
-    // DB7 - DB4 just by setting P1.7 - P1.4 
-    // using a simple assignment
-    LCD_OUT &= ~LCD_PIN_MASK;
-    LCD_OUT |= ((byteToSend & 0x0F) << 4);
-
-    if (isData == TRUE)
-    {
-        LCD_OUT |= LCD_PIN_RS;
-    }
-    else
-    {
-        LCD_OUT &= ~LCD_PIN_RS;
-    }
-
-    // we've set up the input voltages to the LCD.
-    // Now tell it to read them.
-    enable();
+* Description: Turn LCD display on or off
+* Parameters
+*    - isON: 1 if desire to turn on display, 0 otherwise
 */
+void displayON(int isON)
+{
+  if(isON == TRUE)
+    sendByte(0x0F, FALSE);
+  else
+    sendByte(0x08, FALSE);
 }
 
 /*
@@ -145,7 +125,31 @@ void clearDisplay()
 {
     // Clear display, return home
     sendByte(0x01, FALSE);
-    sendByte(0x02, FALSE);
+}
+
+/*
+* Description: Set the position of the cursor on the screen
+* Parameters
+*    - row: zero based row number
+*    - col: zero based column number
+*/
+void lcdSetCursorPosition(char row, char col)
+{
+    char address;
+
+    // construct address from (row, col) pair
+    if (row == 0)
+    {
+        address = 0;
+    }
+    else
+    {
+        address = 0x40;
+    }
+
+    address |= col;
+
+    sendByte(0x80 | address, FALSE);
 }
 
 /*
@@ -156,38 +160,38 @@ void clearDisplay()
 */
 void initLCD(void)
 {
+    LCD_COM_DIR |= LCD_VCC;
+    LCD_COM_OUT |= LCD_VCC;
+    
+    __delay_cycles(45000);
+  
     // set the MSP pin configurations
     // and bring them to low
     LCD_DIR |= LCD_PIN_MASK;
-    LCD_OUT |= LCD_PIN_MASK;
+    LCD_OUT &= ~LCD_PIN_MASK;
     LCD_COM_DIR |= LCD_COM_MASK;
-    LCD_COM_OUT |= LCD_COM_MASK;
+    LCD_COM_OUT &= ~LCD_COM_MASK;
 
     // wait for the LCD to warm up and reach
     // active regions. Remember MSPs can power
     // up much faster than the LCD.
-    __delay_cycles(100000);
+    __delay_cycles(10000);
 
     // initialize the LCD module
-    // Set 8-bit input first line
-    sendByte(0x30, FALSE);
-    
-    // set 4-bit input second line
+    // set 8-bit 2-line
     sendByte(0x38, FALSE);
     
-    // turn LCD off
-    sendByte(0x08, FALSE);
-    
-    clearDisplay();
-
-    
     // Display on, cursor on, blink cursor
-    sendByte(0x0F, FALSE);
+    displayON(TRUE);
     
-    // Cursor move auto-increment
-    sendByte(0x06, FALSE);
+    // clear display
+    clearDisplay();
     
-    sendByte(0x65, TRUE);
+//    sendByte(0x65, TRUE);               // e
+//    sendByte(0x66, TRUE);               // f
+//    sendByte(0x67, TRUE);               // g
+//    sendByte(0x68, TRUE);               // h
+    
 }
 
 /*
@@ -198,14 +202,19 @@ void initLCD(void)
 void mssgDisplay(char *text)
 {
     char *c;
-
+    int count = 16;
     c = text;
 
     // Send characters one by one until string is empty
     while ((c != 0) && (*c != 0))
     {
+        // after 16 characters start writing in second line
+        if(count == 0)
+          lcdSetCursorPosition(1,0);
+      
         sendByte(*c, TRUE);
         c++;
+        count--;
     }
 }
 
@@ -219,13 +228,17 @@ int main(void)
 
     initLCD();
 
-    mssgDisplay("Hello World!");
-
+    mssgDisplay("Center Freq: ###Charge: %%%");
+    
+    __delay_cycles(3000000);
+    
+    LCD_COM_OUT &= ~LCD_VCC;                    // Turn Off LCD
+/*
     while (1)
     {
         __delay_cycles(1000);
     }
-
-//    __bis_SR_register(LPM0_bits);                 // CPU off, enable interrupts
+*/
+    __bis_SR_register(LPM0_bits);               // CPU Low-Power Mode 0
     
 }
