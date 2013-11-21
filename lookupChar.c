06ab726a14e55a66bc2eef48a39f6c28c9d65b25
SignalShield <arturo.torres@upr.edu>
2013-11-21 05:19:20 -0800

    Create lookupChar.c
    Used for converting hex value given from Fuel Gauge to a character to be presented in LCD. Very lazy way to do it. Could be changed for an actual function instead of a "lookup table".

---
+++ lookupChar.c
@@ -0,0 +1,320 @@
+#include <lookupChar.h>
+
+/*
+*  Description: Incredibely lazy way to transform a hex to character from
+*               0 to 100.
+*/
+
+char value[3];
+
+char[] getChar(char hex)
+{
+  switch(hex)
+  {
+	case 0x00:
+	  value[] = '00';
+	  break;
+	case 0x01:
+	  value[] = '01';
+	  break;
+	case 0x02:
+	  value[] = '02';
+	  break;
+	case 0x03:
+	  value[] = '03';
+	  break;
+	case 0x04:
+	  value[] = '04';
+	  break;
+	case 0x05:
+	  value[] = '05';
+	  break;
+	case 0x06:
+	  value[] = '06';
+	  break;
+	case 0x07:
+	  value[] = '07';
+	  break;
+	case 0x08:
+	  value[] = '08';
+	  break;
+	case 0x09:
+	  value[] = '09';
+	  break;
+	case 0x0A:
+	  value[] = '10';
+	  break;
+	case 0x0B:
+	  value[] = '11';
+	  break;
+	case 0x0C:
+	  value[] = '12';
+	  break;
+	case 0x0D:
+	  value[] = '13';
+	  break;
+	case 0x0E:
+	  value[] = '14';
+	  break;
+	case 0x0F:
+	  value[] = '15';
+	  break;
+	case 0x10:
+	  value[] = '16';
+	  break;
+	case 0x11:
+	  value[] = '17';
+	  break;
+	case 0x12:
+	  value[] = '18';
+	  break;
+	case 0x13:
+	  value[] = '19';
+	  break;
+	case 0x14:
+	  value[] = '20';
+	  break;
+	case 0x15:
+	  value[] = '21';
+	  break;
+	case 0x16:
+	  value[] = '22';
+	  break;
+	case 0x17:
+	  value[] = '23';
+	  break;
+	case 0x18:
+	  value[] = '24';
+	  break;
+	case 0x19:
+	  value[] = '25';
+	  break;
+	case 0x1A:
+	  value[] = '26';
+	  break;
+	case 0x1B:
+	  value[] = '27';
+	  break;
+	case 0x1C:
+	  value[] = '28';
+	  break;
+	case 0x1D:
+	  value[] = '29';
+	  break;
+	case 0x1E:
+	  value[] = '30';
+	  break;
+	case 0x1F:
+	  value[] = '31';
+	  break;
+	case 0x20:
+	  value[] = '32';
+	  break;
+	case 0x21:
+	  value[] = '33';
+	  break;
+	case 0x22:
+	  value[] = '34';
+	  break;
+	case 0x23:
+	  value[] = '35';
+	  break;
+	case 0x24:
+	  value[] = '36';
+	  break;
+	case 0x25:
+	  value[] = '37';
+	  break;
+	case 0x26:
+	  value[] = '38';
+	  break;
+	case 0x27:
+	  value[] = '39';
+	  break;
+	case 0x28:
+	  value[] = '40';
+	  break;
+	case 0x29:
+	  value[] = '41';
+	  break;
+	case 0x2A:
+	  value[] = '42';
+	  break;
+	case 0x2B:
+	  value[] = '43';
+	  break;
+	case 0x2C:
+	  value[] = '44';
+	  break;
+	case 0x2D:
+	  value[] = '45';
+	  break;
+	case 0x2E:
+	  value[] = '46';
+	  break;
+	case 0x2F:
+	  value[] = '47';
+	  break;
+	case 0x30:
+	  value[] = '48';
+	  break;
+	case 0x31:
+	  value[] = '49';
+	  break;
+	case 0x32:
+	  value[] = '50';
+	  break;
+	case 0x33:
+	  value[] = '51';
+	  break;
+	case 0x34:
+	  value[] = '52';
+	  break;
+	case 0x35:
+	  value[] = '53';
+	  break;
+	case 0x36:
+	  value[] = '54';
+	  break;
+	case 0x37:
+	  value[] = '55';
+	  break;
+	case 0x38:
+	  value[] = '56';
+	  break;
+	case 0x39:
+	  value[] = '57';
+	  break;
+	case 0x3A:
+	  value[] = '58';
+	  break;
+	case 0x3B:
+	  value[] = '59';
+	  break;
+	case 0x3C:
+	  value[] = '60';
+	  break;
+	case 0x3D:
+	  value[] = '61';
+	  break;
+	case 0x3E:
+	  value[] = '62';
+	  break;
+	case 0x3F:
+	  value[] = '63';
+	  break;
+	case 0x40:
+	  value[] = '64';
+	  break;
+	case 0x41:
+	  value[] = '65';
+	  break;
+	case 0x42:
+	  value[] = '66';
+	  break;
+	case 0x43:
+	  value[] = '67';
+	  break;
+	case 0x44:
+	  value[] = '68';
+	  break;
+	case 0x45:
+	  value[] = '69';
+	  break;
+	case 0x46:
+	  value[] = '70';
+	  break;
+	case 0x47:
+	  value[] = '71';
+	  break;
+	case 0x48:
+	  value[] = '72';
+	  break;
+	case 0x49:
+	  value[] = '73';
+	  break;
+	case 0x4A:
+	  value[] = '74';
+	  break;
+	case 0x4B:
+	  value[] = '75';
+	  break;
+	case 0x4C:
+	  value[] = '76';
+	  break;
+	case 0x4D:
+	  value[] = '77';
+	  break;
+	case 0x4E:
+	  value[] = '78';
+	  break;
+	case 0x4F:
+	  value[] = '79';
+	  break;
+	case 0x50:
+	  value[] = '80';
+	  break;
+	case 0x51:
+	  value[] = '81';
+	  break;
+	case 0x52:
+	  value[] = '82';
+	  break;
+	case 0x53:
+	  value[] = '83';
+	  break;
+	case 0x54:
+	  value[] = '84';
+	  break;
+	case 0x55:
+	  value[] = '85';
+	  break;
+	case 0x56:
+	  value[] = '86';
+	  break;
+	case 0x57:
+	  value[] = '87';
+	  break;
+	case 0x58:
+	  value[] = '88';
+	  break;
+	case 0x59:
+	  value[] = '89';
+	  break;
+	case 0x5A:
+	  value[] = '90';
+	  break;
+	case 0x5B:
+	  value[] = '91';
+	  break;
+	case 0x5C:
+	  value[] = '92';
+	  break;
+	case 0x5D:
+	  value[] = '93';
+	  break;
+	case 0x5E:
+	  value[] = '94';
+	  break;
+	case 0x5F:
+	  value[] = '95';
+	  break;
+	case 0x60:
+	  value[] = '96';
+	  break;
+	case 0x61:
+	  value[] = '97';
+	  break;
+	case 0x62:
+	  value[] = '98';
+	  break;
+	case 0x63:
+	  value[] = '99';
+	  break;
+	case 0x64:
+	  value[] = '99';
+	  break;
+  }
+
+  return value[];
+}
